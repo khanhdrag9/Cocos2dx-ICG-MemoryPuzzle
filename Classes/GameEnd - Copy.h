00d867cf17be7e88ad880698d2d55f069b9ec4e1
khanhdrag9 <tkhanh783@gmail.com>
2018-11-08 23:01:57 +0700

    + Complete Protype v1

--- Classes/GameEnd - Copy.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#define FONT_TEXT "fonts/arial.ttf"
-//GameStart
-#define TITLE_GAME "Memory Puzzple TNK"
-#define SIZE_TITLE_GAME 80
-#define LABEL_STARTGAME "Click to screen to start"
-#define SIZE_LABEL_STARTGAME 50
